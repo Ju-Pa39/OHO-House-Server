2b631b6ad183e1e47d4a2184048f6a7bd1cb97f8
Ju-Pa39 <pichai.ju39@gmail.com>
2024-12-14 16:37:38 +0700

    API-Contact

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,9 +1,26 @@
+const prisma = require('../config/prisma')
 
-exports.createContact = async(req,res)=> {
-    try{
-        res.send('บันทึกข้อมูลเรียบร้อย')
-    }catch(err){
-        console.log(err)
-        res.status(500).json({message : "Server Error"})
+
+exports.createContact = async (req, res) => {
+    try {
+        const { name, email, phone, message } = req.body;
+
+        if (!name) {
+            return res.status(400).json({ message: "กรุณากรอกชื่อ" });
+        }
+
+        const create = await prisma.contact.create({
+            data: {
+                name,
+                email,
+                phone,
+                message
+            }
+        });
+
+        res.send('บันทึกข้อมูลเรียบร้อย');
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "ข้อผิดพลาดจากเซิร์ฟเวอร์" });
     }
-}
+};
